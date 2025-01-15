a7dc9f95e7979295b9a5d30b353a0766044ba18e
Joseevb <joseevasquez04@gmail.com>
2025-01-15 18:27:10 +0100

    initialized repo

---
+++ src/main/java/es/dws/JVBt06e04/JvBt06e04Application.java
@@ -0,0 +1,13 @@
+package es.dws.JVBt06e04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvBt06e04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvBt06e04Application.class, args);
+	}
+
+}
---
+++ src/test/java/es/dws/JVBt06e04/JvBt06e04ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.dws.JVBt06e04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvBt06e04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
